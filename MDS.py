699e81c93e039317a6c89ffb739dbf256fcac901
Wesley Chen <wesleychen2007nj@gmaail.com>
2026-02-10 19:52:52 -0800

    modified MDS

--- MDS.py
+++ MDS.py
@@ -2,27 +2,33 @@ import sklearn
 import numpy as np
 import matplotlib.pyplot as plt
 
-CSV_data = np.loadtxt('similarity_scores/Trevor.csv', delimiter=',', dtype=str)
+Wesley_data = np.loadtxt('similarity_scores/Wesley.csv', delimiter=',', dtype=str)
+Trevor_data = np.loadtxt('similarity_scores/Trevor.csv', delimiter=',', dtype=str)
 
-labels = CSV_data[1:,0]
-data = np.array(CSV_data[1:,1:], dtype=float)
+labels = Wesley_data[1:,0]
+data = (np.array(Wesley_data[1:,1:], dtype=float) + np.array(Trevor_data[1:,1:], dtype=float))/2
 
-data = np.where(data!=0, 1/data, np.zeros_like(data))
+data = np.where(data>0.5, 1/data, np.zeros_like(data))
+print(data)
 
-cmds = sklearn.manifold.ClassicalMDS(
+mds = sklearn.manifold.MDS(
     n_components=2,
+    max_iter=3000,
+    eps=1e-9,
+    n_init=1,
+    random_state=42,
     metric="precomputed",
+    n_jobs=1,
+    init="classical_mds",
 )
-X_cmds = cmds.fit_transform(data)
+X_mds = mds.fit(data).embedding_
 
 fig = plt.figure(1)
 ax = plt.axes([0.0, 0.0, 1.0, 1.0])
 
-plt.scatter(
-    X_cmds[:, 0], X_cmds[:, 1], color="lightcoral", s=100, lw=0, label="Classical MDS"
-)
+plt.scatter(X_mds[:, 0], X_mds[:, 1], color="turquoise", s=100, lw=0, label="MDS")
 
-for (x, y), label in zip(X_cmds, labels):
+for (x, y), label in zip(X_mds, labels):
     plt.annotate(
         label,
         (x, y),
