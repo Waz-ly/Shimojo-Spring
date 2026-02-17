7beff3d055f733a6c7a456e6ad433f9c1b068933
Wesley Chen <wesleychen2007nj@gmaail.com>
2026-02-16 17:58:06 -0800

    added new stimuli

--- MDS.py
+++ MDS.py
@@ -8,6 +8,8 @@ Trevor_data = np.loadtxt('similarity_scores/Trevor.csv', delimiter=',', dtype=st
 labels = Wesley_data[1:,0]
 data = (np.array(Wesley_data[1:,1:], dtype=float) + np.array(Trevor_data[1:,1:], dtype=float))/2
 
+data = np.array(Trevor_data[1:,1:], dtype=float)
+
 data = np.where(data>0.5, 1/data, np.zeros_like(data))
 print(data)
 
@@ -16,15 +18,15 @@ mds = sklearn.manifold.MDS(
     max_iter=3000,
     eps=1e-9,
     n_init=1,
-    random_state=42,
+    random_state=1,
     metric="precomputed",
     n_jobs=1,
     init="classical_mds",
 )
 X_mds = mds.fit(data).embedding_
 
-fig = plt.figure(1)
-ax = plt.axes([0.0, 0.0, 1.0, 1.0])
+fig = plt.figure()
+ax = fig.add_subplot()
 
 plt.scatter(X_mds[:, 0], X_mds[:, 1], color="turquoise", s=100, lw=0, label="MDS")
 
